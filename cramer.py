214e3e7b551ddc7c7adbcac061ef38fb8a6e8e6d
Daniel Malik <malik@Daniels-Macintosh.local>
2018-01-13 23:50:37 +0100

    matrix dimension check, wrong matrix data, TDD

--- cramer.py
+++ cramer.py
@@ -1,4 +1,6 @@
+import copy
 import pandas
+
 from os import path
 
 import ipdb
@@ -10,12 +12,15 @@ class Cramer:
 
     def __init__(self, file_name):
         self.file_name = file_name
-        self.data = self._get_data_from_file()
+        self.raw_data = self._get_data_from_file()
+        self.data = copy.deepcopy(self.raw_data)
+        self.sums = [int(s.pop('s')) for s in self.data.values()]
+        self.matrix_dimension = None
+        self.matrix_determinant = None
 
     def _get_data_from_file(self):
         try:
             with open(path.join(base_path, self.file_name), 'r') as file:
-
                 data = pandas.read_csv(file, sep=',', dtype=str).to_dict(orient='index')
                 if data:
                     return data
@@ -24,13 +29,36 @@ class Cramer:
         except FileNotFoundError as e:
             print(f'No such file found, {e}, text input: {self.file_name}')
 
+    def _check_if_matrix_is_square(self):
+        matrix_dimension_set = {len(self.data)}
+        if matrix_dimension_set == {len(d) for d in self.data.values()}:
+            self.matrix_dimension, = matrix_dimension_set
+            return True
+        else:
+            raise Exception(f'{self.data} is not a n dimensional square matrix')
+
+    def _check_if_determinant_is_not_zero(self):
+        return False
+
+    def check_if_cramers_rule_assumptions_are_met(self):
+        try:
+            if self._check_if_matrix_is_square():
+                if self._check_if_determinant_is_not_zero():
+                    return True
+        except:
+            print('wrong')
+
     @classmethod
     def from_input(cls):
         return cls(input('enter file_name: '))
 
 
 if __name__ == '__main__':
+    # process = Cramer('test_1.csv')
     process = Cramer.from_input()
     print(process.file_name)
+    print(process.raw_data)
     print(process.data)
+    print(process.sums)
+    process.check_if_cramers_rule_assumptions_are_met()
     # ipdb.set_trace()
--- tests.py
+++ tests.py
@@ -8,8 +8,36 @@ class TestCramer:
     def test_open_file_and_create_data(self):
         process = cramer.Cramer('test_1.csv')
         assert process.file_name == 'test_1.csv'
-        assert process.data == {
+        assert process.raw_data == {
             0: {'x': '2', 'y': '5', 'z': '3', 's': '5'},
             1: {'x': '4', 'y': '2', 'z': '5', 's': '4'},
             2: {'x': '3', 'y': '8', 'z': '4', 's': '9'},
         }
+        assert process.data == {
+            0: {'x': '2', 'y': '5', 'z': '3'},
+            1: {'x': '4', 'y': '2', 'z': '5'},
+            2: {'x': '3', 'y': '8', 'z': '4'},
+        }
+
+    def test_creating_subarrays(self):
+        process = cramer.Cramer('test_1.csv')
+        assert process.sums == [5, 4, 9]
+
+    def test_check_if_matrix_is_n_dimensional_square_matrix(self):
+        process = cramer.Cramer('test_1.csv')
+        assert process._check_if_matrix_is_square() is True
+        assert process.matrix_dimension == 3
+
+    def test_check_non_square_matrix(self):
+        process = cramer.Cramer('wrong.csv')
+        with pytest.raises(Exception):
+            process._check_if_matrix_is_square()
+
+    def test_determinant_is_not_zero(self):
+        process = cramer.Cramer('test_1.csv')
+        assert process._check_if_determinant_is_not_zero() is True
+        assert process.matrix_determinant == -2
+
+    def test_if_cramers_rule_can_be_applied(self):
+        process = cramer.Cramer('test_1.csv')
+        assert process.check_if_cramers_rule_assumptions_are_met() is True
